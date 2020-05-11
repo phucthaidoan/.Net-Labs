4aca21183dd9bde62441cf84ed62def5e54ba9b6
Phuc <phuc.thai@saigontechnology.com>
2020-05-12 00:02:55 +0700

    Added Work Queue sample

---
+++ RabbitMQ-GettingStarted/Readme.md
@@ -0,0 +1,21 @@
+# How to test
+
+Foobar is a Python library for dealing with word pluralization.
+
+## Run two Worker instances at the same time
+
+
+## Publish new tasks
+
+```python
+dotnet run "First message."
+dotnet run "Second message.."
+dotnet run "Third message..."
+```
+
+## Result
+```
+ [*] Waiting for messages.
+ Press [enter] to exit.
+ [x] Received Second message..
+ [x] Done
---
+++ RabbitMQ-GettingStarted/Worker/Worker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Worker
+{
+    class Worker
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "task_queue",
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                Console.WriteLine(" [*] Waiting for messages.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body.ToArray());
+                    Console.WriteLine(" [x] Received {0}", message);
+
+                    int dots = message.Split('.').Length - 1;
+                    Thread.Sleep(dots * 1000);
+
+                    Console.WriteLine(" [x] Done");
+                };
+                channel.BasicConsume(queue: "task_queue", autoAck: true, consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
