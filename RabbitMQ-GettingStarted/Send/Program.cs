6b5c7f8158d0e7a998d51b860f7c155324222b5d
Phuc <phuc.thai@saigontechnology.com>
2020-05-11 23:16:35 +0700

    Init sample

---
+++ RabbitMQ-GettingStarted/Send/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Send
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    var message = "Hello world!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: "hello",
+                        basicProperties: null,
+                        body: body);
+
+                    Console.WriteLine("[x] Sent {0}", message);
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
